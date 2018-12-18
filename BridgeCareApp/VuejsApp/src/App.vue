914c6e6e86f5c93addfb6271decb2c8f24ee261d
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2018-12-18 11:22:15 -0600

    minor changes

--- BridgeCareApp/VuejsApp/src/App.vue
+++ BridgeCareApp/VuejsApp/src/App.vue
@@ -115,7 +115,7 @@
                 },
                 {
                     cacheLocation: 'localStorage',
-                    redirectUri: redirectUri
+                    //redirectUri: redirectUri
                 }
             );
         }
