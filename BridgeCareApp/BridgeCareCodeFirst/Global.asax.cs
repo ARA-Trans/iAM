8025ffa438600ee75f192d5aac796398e8347312
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-01-30 12:54:03 -0600

    minor changes

--- BridgeCareApp/BridgeCareCodeFirst/Global.asax.cs
+++ BridgeCareApp/BridgeCareCodeFirst/Global.asax.cs
@@ -8,11 +8,10 @@ using System.Web.Routing;
 
 namespace BridgeCare
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
-            //AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
