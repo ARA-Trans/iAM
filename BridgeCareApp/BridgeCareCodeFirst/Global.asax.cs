75edb176dbf557b880ae637823bb12fc163c78f0
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-02-01 17:29:42 -0600

    minor changes

--- BridgeCareApp/BridgeCareCodeFirst/Global.asax.cs
+++ BridgeCareApp/BridgeCareCodeFirst/Global.asax.cs
@@ -13,8 +13,6 @@ namespace BridgeCare
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
