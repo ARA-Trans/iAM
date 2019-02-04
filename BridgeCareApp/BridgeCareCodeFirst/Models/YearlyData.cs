959a693f000eedf05af51dd303864ce08dc50eff
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-02-04 10:56:29 -0600

    Removed old folder

--- BridgeCareApp/BridgeCareCodeFirst/Models/YearlyData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BridgeCare.Models
-{
-    public class YearlyData
-    {
-        public int Year { get; set; }
-        public double? Amount { get; set; }
-        public string BudgetName { get; set; }
-    }
-}
