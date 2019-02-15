d7ab072fe5e25cce58ae73568a7d50026c7f911b
rbyrne2 <46910316+rbyrne2@users.noreply.github.com>
2019-02-15 10:02:31 -0500

    add ability to get all sectitonIDs in a network
    POST call receives network model, query made on SECTION_<network> table and get the text based id's BridgeID (called SECTION) and BridgeKey (called FACILITY) and also retrieves integer SECTIONID which in key in simulation tables

---
+++ BridgeCareApp/BridgeCare/DataAccessLayer/Sections.cs
@@ -0,0 +1,40 @@
+﻿using BridgeCare.ApplicationLog;
+using BridgeCare.Interfaces;
+using BridgeCare.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace BridgeCare.DataAccessLayer
+{
+    public class Sections : ISections
+    {
+        public Sections() { }
+        public IQueryable<SectionModel> GetSections(NetworkModel data, BridgeCareContext db)
+        {
+            IQueryable < SectionModel > rawQueryForData = null;
+            var select =
+                    "SELECT Sectionid,Facility, Section" +
+                        " FROM Section_" + data.NetworkId +
+                        " Order By Facility, Section";
+
+            try
+            {
+                rawQueryForData = db.Database.SqlQuery<SectionModel>(select).AsQueryable();
+            }
+            catch (SqlException ex)
+            {
+                HandleException.SqlError(ex, "Section_");
+            }
+            catch (OutOfMemoryException ex)
+            {
+                HandleException.OutOfMemoryError(ex);
+            }
+            return rawQueryForData;
+        }
+    }
+    
+}
---
+++ BridgeCareApp/BridgeCare/Models/SectionModel.cs
@@ -0,0 +1,19 @@
+﻿using BridgeCare.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BridgeCare.Models
+{
+    public class SectionModel
+    {
+        public int SectionId { get; set; }
+        public Char[] BridgeKey { get; set; }
+        public Char[] BridgeiId { get; set; }
+
+        //isnt this needed, data is valid only for a specific network
+        //can be broken easily
+        //public NetworkModel Network { get; set; }
+    }
+}
