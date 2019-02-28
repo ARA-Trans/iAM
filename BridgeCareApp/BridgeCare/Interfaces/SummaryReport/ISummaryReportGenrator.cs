d7420249b603223c64e1979e51f9d1f75cf644ca
rbyrne2 <46910316+rbyrne2@users.noreply.github.com>
2019-02-28 14:59:41 -0500

    did not get this in merge but it is in develop

---
+++ BridgeCareApp/BridgeCare/Interfaces/SummaryReport/ISummaryReportGenrator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BridgeCare.Models;
+
+namespace BridgeCare.Interfaces
+{
+    public interface ISummaryReportGenerator
+    {
+        byte[] GenerateExcelReport(SimulationModel simulationModel);
+    }
+}
