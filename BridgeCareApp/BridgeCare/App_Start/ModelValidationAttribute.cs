c18f22bea63975da587e521d2fd0772eeb75ea58
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-03-08 14:35:05 -0600

    Added an overload method to pass in the message for ModelState

--- BridgeCareApp/BridgeCare/App_Start/ModelValidationAttribute.cs
+++ BridgeCareApp/BridgeCare/App_Start/ModelValidationAttribute.cs
@@ -12,20 +12,26 @@ namespace System.Web.Http.Filters
         {
             if (_validate(actionContext.ActionArguments))
             {
+                var testb = actionContext.ActionDescriptor;
                 actionContext.Response = actionContext.Request.CreateErrorResponse(
                 HttpStatusCode.BadRequest, "The argument cannot be null");
             }
             if (!actionContext.ModelState.IsValid)
             {
                 actionContext.Response = actionContext.Request.CreateErrorResponse(
-                HttpStatusCode.BadRequest, actionContext.ModelState);
+                HttpStatusCode.BadRequest, Message);
             }
         }
 
         private readonly Func<Dictionary<string, object>, bool> _validate;
+        private string Message { get; set; }
 
         public ModelValidationAttribute() : this(arguments => arguments.ContainsValue(null))
         { }
+        public ModelValidationAttribute(string message) : this(arguments => arguments.ContainsValue(null))
+        {
+            Message = message;
+        }
 
         public ModelValidationAttribute(Func<Dictionary<string, object>, bool> checkCondition)
         {
