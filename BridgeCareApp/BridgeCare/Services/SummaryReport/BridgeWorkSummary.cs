5f7d6204344a1f41cfbf9d326b81fbb532965196
Amruta Borgaonkar <aborgaonkar@ara.com>
2019-03-19 10:54:01 -0500

    Feature/31 (#84)
    * work in progress.
    
    * 1st 3 sections are done.
    
    * In progress work. Added 2 more sections and some reusable code for next section.
    
    * Done with 7 sections of work summary tab.
    
    * 2 sections remaining, added some refactoring as well.
    
    * Done with Bridge Work Summary Tab
    
    * Removed TODO as already taken care.
    
    * Corrected method name.
    
    * Added number formatting to Total Deck Area(missed this out)
    
    * Updated PR with removing static classes, adding resources file to alternative to constants.
    Removed region declarations.
    
    * Renamed to correct case names.
    
    * Updated - made all classes non static.
    
    * Useful to Used.

---
+++ BridgeCareApp/BridgeCare/Services/SummaryReport/BridgeWorkSummary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using BridgeCare.Models;
+using OfficeOpenXml;
+
+namespace BridgeCare.Services
+{
+    public class BridgeWorkSummary
+    {
+        private readonly CostBudgetsWorkSummary costBudgetsWorkSummary;
+        private readonly BridgesCulvertsWorkSummary bridgesCulvertsWorkSummary;
+        private readonly BridgeRateDeckAreaWorkSummary bridgeRateDeckAreaWorkSummary;
+
+        public BridgeWorkSummary(CostBudgetsWorkSummary costBudgetsWorkSummary, BridgesCulvertsWorkSummary bridgesCulvertsWorkSummary, BridgeRateDeckAreaWorkSummary bridgeRateDeckAreaWorkSummary)
+        {
+            this.costBudgetsWorkSummary = costBudgetsWorkSummary ?? throw new ArgumentNullException(nameof(costBudgetsWorkSummary));
+            this.bridgesCulvertsWorkSummary = bridgesCulvertsWorkSummary ?? throw new ArgumentNullException(nameof(bridgesCulvertsWorkSummary));
+            this.bridgeRateDeckAreaWorkSummary = bridgeRateDeckAreaWorkSummary ?? throw new ArgumentNullException(nameof(bridgeRateDeckAreaWorkSummary));
+        }
+
+        public void Fill(ExcelWorksheet worksheet, List<SimulationDataModel> simulationDataModels, List<int> simulationYears, BridgeCareContext dbContext)
+        {
+            var currentCell = new CurrentCell { Row = 1, Column = 1 };
+
+            costBudgetsWorkSummary.FillCostBudgetWorkSummarySections(worksheet, currentCell, simulationYears, simulationDataModels);
+
+            bridgesCulvertsWorkSummary.FillBridgesCulvertsWorkSummarySections(worksheet, currentCell, simulationYears, simulationDataModels);
+
+            bridgeRateDeckAreaWorkSummary.FillBridgeRateDeckAreaWorkSummarySections(worksheet, currentCell, simulationYears, simulationDataModels);
+
+            worksheet.Calculate();
+            worksheet.Cells.AutoFitColumns();
+        }        
+    }
+}
---
+++ BridgeCareApp/BridgeCare/Services/SummaryReport/BridgeWorkSummaryHelper.cs
@@ -0,0 +1,110 @@
+﻿using BridgeCare.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BridgeCare.Services
+{
+    public class BridgeWorkSummaryComputationHelper
+    {
+        const string NoTreatment = "No Treatment";
+
+        public double CalculateCost(List<SimulationDataModel> simulationDataModels, int year, string project)
+        {
+            double cost = 0;
+            foreach (var simulationDataModel in simulationDataModels)
+            {
+                var yearData = simulationDataModel.YearsData.Find(y => y.Year == year && y.Project == project);
+                cost = cost + (yearData != null ? yearData.Cost : 0);
+            }
+
+            return cost;
+        }
+
+        public int CalculateNoTreatmentCountForCulverts(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.Project == NoTreatment && !y.CulvD.Equals("N"))).Count;
+        }
+
+        public int CalculatePreservationPoorFixCount(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.Culv == "Y" && y.SD == "N")).Count;
+        }
+
+        public int CalculateCountByProject(List<SimulationDataModel> simulationDataModels, int year, string project)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.Project == project)).Count;
+        }
+
+        public int CalculateNoTreatmentCountForBridges(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.Project == NoTreatment && y.CulvD.Equals("N"))).Count;
+        }
+
+        public int CalculatePoorBridgeCount(List<SimulationDataModel> simulationDataModels, int year, string type)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.PoorOnOffRate == type)).Count;
+        }
+
+        public int CalculateTotalPoorBridgesCount(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && y.SD == "Y")).Count;
+        }
+
+        public double CalculateTotalPoorBridgesDeckArea(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            double sum = 0;            
+            foreach (var simulationDataModel in simulationDataModels)
+            {
+                var yearData = simulationDataModel.YearsData.Find(y => y.Year == year && y.SD == "Y");
+                sum = sum + (yearData != null ? Convert.ToDouble(simulationDataModel.DeckArea) : 0);
+            }
+
+            return sum;
+        }
+
+        public  int CalculateTotalBridgeGoodCount(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && Convert.ToDouble(y.MinC) >= 7)).Count;
+        }
+
+        public int CalculateTotalBridgePoorCount(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            return simulationDataModels.FindAll(s => s.YearsData.Exists(y => y.Year == year && Convert.ToDouble(y.MinC) < 5)).Count;
+        }
+
+        public double CalculateTotalGoodDeckArea(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            double sum = 0;
+            foreach (var simulationDataModel in simulationDataModels)
+            {
+                var yearData = simulationDataModel.YearsData.Find(y => y.Year == year && Convert.ToDouble(y.MinC) >= 7);
+                sum = sum + (yearData != null ? Convert.ToDouble(simulationDataModel.DeckArea) : 0);
+            }
+
+            return sum;
+        }
+
+        public double CalculateTotalPoorDeckArea(List<SimulationDataModel> simulationDataModels, int year)
+        {
+            double sum = 0;
+            foreach (var simulationDataModel in simulationDataModels)
+            {
+                var yearData = simulationDataModel.YearsData.Find(y => y.Year == year && Convert.ToDouble(y.MinC) < 5);
+                sum = sum + (yearData != null ? Convert.ToDouble(simulationDataModel.DeckArea) : 0);
+            }
+
+            return sum;
+        }
+
+        public double CalculateTotalDeckArea(List<SimulationDataModel> simulationDataModels)
+        {
+            double sum = 0;
+            foreach (var simulationDataModel in simulationDataModels)
+            {
+                sum = sum + Convert.ToDouble(simulationDataModel.DeckArea);
+            }
+
+            return sum;
+        }
+    }
+}
