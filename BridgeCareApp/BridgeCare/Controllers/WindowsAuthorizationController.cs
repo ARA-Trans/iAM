66ab5014f28d748b9cdd28c4f3c9b9c9734914eb
saransh bhardwaj <saranshbhardwaj@gmail.com>
2019-03-21 09:37:09 -0500

    Feature/92 added windows authentication and removed Azure AD B2C auth (#102)
    * added windows authentication and removed AzureAD B2C authentication
    
    * removed duplicate folder and commented code
    
    * changed class name
    
    * refactored TopNavBar component to make it cleaner
    
    * removed async from created hook

---
+++ BridgeCareApp/BridgeCare/Controllers/WindowsAuthorizationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace BridgeCare.Controllers
+{
+    [Authorize]
+    [RoutePrefix("auth")]
+    public class WindowsAuthorizationController : ApiController
+    {
+        [HttpGet]
+        [Route("getuser")]
+        [EnableCors(origins: "*", headers: "*", methods: "*", SupportsCredentials = true)]
+        public IHttpActionResult GetUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Ok($"{User.Identity.Name}");
+            }
+            else
+            {
+                return BadRequest("Not authenticated");
+            }
+        }
+    }
+}
