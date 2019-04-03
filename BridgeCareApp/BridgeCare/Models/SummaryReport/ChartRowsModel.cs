524c645ececd34cfbfd50b0d7f2ebe68b3459ff0
Amruta Borgaonkar <aborgaonkar@ara.com>
2019-04-03 15:44:42 -0500

    Feature/32 (#119)
    * Work in progress. Almost done.
    
    * Updated with styling and organised code.

---
+++ BridgeCareApp/BridgeCare/Models/SummaryReport/ChartRowsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BridgeCare.Models
+{
+    public class ChartRowsModel
+    {
+        public int TotalBridgeCountSectionYearsRow { get; set; }
+    }
+}
