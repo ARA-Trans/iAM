752b5c951735adde11ef5048ca22c6b4aceb565e
Amruta Borgaonkar <aborgaonkar@ara.com>
2019-04-05 09:51:00 -0500

    Feature/33 (#123)
    * Work in progress. Almost done.
    
    * Updated with styling and organised code.
    
    * Initial work.
    
    * Done with 4th tab report implementation.
    Moved relevant common code to new class for 3rd and 4th tab reports.
    
    * Removed static methods(gets created when used VS refactoring)
    
    * Commented input that was set for testing purpose.

--- BridgeCareApp/BridgeCare/Services/SummaryReport/ConditionBridgeCount.cs
+++ BridgeCareApp/BridgeCare/Services/SummaryReport/ConditionBridgeCount.cs
@@ -2,11 +2,19 @@
 using OfficeOpenXml.Drawing.Chart;
 using System.Drawing;
 using OfficeOpenXml.Style;
+using System;
 
 namespace BridgeCare.Services
 {
     public class ConditionBridgeCount
-    {        
+    {
+        private readonly StackedColumnChartCommon stackedColumnChartCommon;
+
+        public ConditionBridgeCount(StackedColumnChartCommon stackedColumnChartCommon)
+        {
+            this.stackedColumnChartCommon = stackedColumnChartCommon ?? throw new ArgumentNullException(nameof(stackedColumnChartCommon));
+        }
+
         /// <summary>
         /// Fill Condition Bridge Cnt tab report.
         /// </summary>
@@ -16,56 +24,43 @@ namespace BridgeCare.Services
         /// <param name="count"></param>
         public void Fill(ExcelWorksheet worksheet, ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalBridgeCountSectionYearsRow, int count)
         {
+            SetWorksheetProperties(worksheet);
+            var title = Properties.Resources.ConditionByBridgeCount;
+            var chart = worksheet.Drawings.AddChart(title, eChartType.ColumnStacked);
+            stackedColumnChartCommon.SetChartProperties(chart, title);
 
-            worksheet.Cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
-            worksheet.Cells.Style.Fill.BackgroundColor.SetColor(Color.LightGray);
-            worksheet.Protection.IsProtected = true;
-            worksheet.View.ShowHeaders = false;
-
-            var chart = worksheet.Drawings.AddChart(Properties.Resources.ConditionByBridgeCount, eChartType.ColumnStacked);
-            SetChartProperties(chart);
-
-            SetChartAxes(chart);
+            stackedColumnChartCommon.SetChartAxes(chart);
             AddSeries(bridgeWorkSummaryWorkSheet, totalBridgeCountSectionYearsRow, count, chart);
 
             chart.AdjustPositionAndSize();
             chart.Locked = true;
         }
 
-        private void SetChartProperties(ExcelChart chart)
+        private void SetWorksheetProperties(ExcelWorksheet worksheet)
         {
-            chart.Title.Text = Properties.Resources.ConditionByBridgeCount;
-            chart.SetPosition(10, 0, 10, 0);
-            chart.SetSize(800, 600);
-            chart.RoundedCorners = false;
-            chart.PlotArea.Border.Fill.Style = eFillStyle.NoFill;
-            chart.Legend.Position = eLegendPosition.Bottom;
+            var excelFill = worksheet.Cells.Style.Fill;
+            excelFill.PatternType = ExcelFillStyle.Solid;
+            excelFill.BackgroundColor.SetColor(Color.LightGray);
+            worksheet.Protection.IsProtected = true;
+            worksheet.View.ShowHeaders = false;
         }
 
-        private static void AddSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalBridgeCountSectionYearsRow, int count, ExcelChart chart)
+        private void AddSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalBridgeCountSectionYearsRow, int count, ExcelChart chart)
         {
             CreateSeries(bridgeWorkSummaryWorkSheet, totalBridgeCountSectionYearsRow, count, chart, totalBridgeCountSectionYearsRow + 3, Properties.Resources.Poor, Color.Red);
+
             CreateSeries(bridgeWorkSummaryWorkSheet, totalBridgeCountSectionYearsRow, count, chart, totalBridgeCountSectionYearsRow + 2, Properties.Resources.Fair, Color.Yellow);
+
             CreateSeries(bridgeWorkSummaryWorkSheet, totalBridgeCountSectionYearsRow, count, chart, totalBridgeCountSectionYearsRow + 1, Properties.Resources.Good, Color.Green);
         }
 
-        private static void CreateSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalBridgeCountSectionYearsRow, int count, ExcelChart chart, int fromRow, string header, Color color)
+        private void CreateSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalBridgeCountSectionYearsRow, int count, ExcelChart chart, int fromRow, string header, Color color)
         {
-            var serie = chart.Series.Add(bridgeWorkSummaryWorkSheet.Cells[fromRow, 2, fromRow, count + 2], bridgeWorkSummaryWorkSheet.Cells[totalBridgeCountSectionYearsRow, 2, totalBridgeCountSectionYearsRow, count + 2]);
-            serie.Header = header;
-            serie.Fill.Color = color;
-        }
-
-        private void SetChartAxes(ExcelChart chart)
-        {
-            chart.XAxis.MinorTickMark = eAxisTickMark.None;
-            chart.XAxis.MajorTickMark = eAxisTickMark.None;
-            chart.XAxis.Border.Fill.Style = eFillStyle.NoFill;
-
-            chart.YAxis.MinorTickMark = eAxisTickMark.None;
-            chart.YAxis.MajorTickMark = eAxisTickMark.None;
-            chart.YAxis.MajorGridlines.Fill.Color = Color.LightGray;
-            chart.YAxis.Border.Fill.Style = eFillStyle.NoFill;
+            var serie = bridgeWorkSummaryWorkSheet.Cells[fromRow, 2, fromRow, count + 2];
+            var xSerie = bridgeWorkSummaryWorkSheet.Cells[totalBridgeCountSectionYearsRow, 2, totalBridgeCountSectionYearsRow, count + 2];
+            var excelChartSerie = chart.Series.Add(serie, xSerie);
+            excelChartSerie.Header = header;
+            excelChartSerie.Fill.Color = color;
         }
     }
 }
---
+++ BridgeCareApp/BridgeCare/Services/SummaryReport/ConditionDeckArea.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml.Style;
+
+namespace BridgeCare.Services
+{
+    public class ConditionDeckArea
+    {
+        private readonly StackedColumnChartCommon stackedColumnChartCommon;
+
+        public ConditionDeckArea(StackedColumnChartCommon stackedColumnChartCommon)
+        {
+            this.stackedColumnChartCommon = stackedColumnChartCommon ?? throw new ArgumentNullException(nameof(stackedColumnChartCommon));
+        }
+
+        /// <summary>
+        /// Fill Condition DA tab report
+        /// </summary>
+        /// <param name="worksheet"></param>
+        /// <param name="bridgeWorkSummaryWorkSheet"></param>
+        /// <param name="totalDeckAreaSectionYearsRow"></param>
+        /// <param name="count"></param>
+        public void Fill(ExcelWorksheet worksheet, ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalDeckAreaSectionYearsRow, int count)
+        {
+            SetWorksheetProperties(worksheet);
+            var title = Properties.Resources.ConditionByDeckArea;
+            var chart = worksheet.Drawings.AddChart(title, eChartType.ColumnStacked);
+            stackedColumnChartCommon.SetChartProperties(chart, title);
+
+            SetChartAxes(chart);
+            AddSeries(bridgeWorkSummaryWorkSheet, totalDeckAreaSectionYearsRow, count, chart);
+
+            chart.AdjustPositionAndSize();
+            chart.Locked = true;
+        }
+
+        private void SetWorksheetProperties(ExcelWorksheet worksheet)
+        {
+            var excelFill = worksheet.Cells.Style.Fill;
+            excelFill.PatternType = ExcelFillStyle.Solid;
+            excelFill.BackgroundColor.SetColor(Color.LightGray);
+            worksheet.Protection.IsProtected = true;
+            worksheet.View.ShowHeaders = false;
+        }
+
+        private void AddSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalDeckAreaSectionYearsRow, int count, ExcelChart chart)
+        {
+            CreateSeries(bridgeWorkSummaryWorkSheet, totalDeckAreaSectionYearsRow, count, chart, totalDeckAreaSectionYearsRow + 3, Properties.Resources.Poor, Color.Red);
+
+            CreateSeries(bridgeWorkSummaryWorkSheet, totalDeckAreaSectionYearsRow, count, chart, totalDeckAreaSectionYearsRow + 2, Properties.Resources.Fair, Color.Yellow);
+
+            CreateSeries(bridgeWorkSummaryWorkSheet, totalDeckAreaSectionYearsRow, count, chart, totalDeckAreaSectionYearsRow + 1, Properties.Resources.Good, Color.Green);
+        }
+
+        private void CreateSeries(ExcelWorksheet bridgeWorkSummaryWorkSheet, int totalDeckAreaSectionYearsRow, int count, ExcelChart chart, int fromRow, string header, Color color)
+        {
+            var serie = bridgeWorkSummaryWorkSheet.Cells[fromRow, 2, fromRow, count + 2];
+            var xSerie = bridgeWorkSummaryWorkSheet.Cells[totalDeckAreaSectionYearsRow, 2, totalDeckAreaSectionYearsRow, count + 2];
+            var excelChartSerie = chart.Series.Add(serie, xSerie);
+            excelChartSerie.Header = header;
+            excelChartSerie.Fill.Color = color;
+        }  
+
+        private void SetChartAxes(ExcelChart chart)
+        {
+            stackedColumnChartCommon.SetChartAxes(chart);
+            var yAxis = chart.YAxis;
+            yAxis.DisplayUnit = 1000000;
+            yAxis.Format = "_(* #,##0.00_);_(* (#,##0.00);_(* '-'??_);_(@_)";
+            yAxis.Title.TextVertical = OfficeOpenXml.Drawing.eTextVerticalType.Vertical;
+            yAxis.Title.Font.Size = 10;
+            yAxis.Title.Text = "Millions sqft";
+        }
+    }
+}
