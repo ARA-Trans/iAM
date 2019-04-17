57a0eea9763ee17b6ace8415b670554f44d6adf0
Amruta Borgaonkar <aborgaonkar@ara.com>
2019-04-17 10:28:36 -0500

    Removed comment.

--- BridgeCareApp/BridgeCare/Models/SummaryReport/SimulationDataModel.cs
+++ BridgeCareApp/BridgeCare/Models/SummaryReport/SimulationDataModel.cs
@@ -10,8 +10,7 @@ namespace BridgeCare.Models
 
         // Used in some tab reports
         public string DeckArea { get; set; }
-
-        // Added to simplify further usage
+                
         public int BRKey { get; set; }
     }
 }
