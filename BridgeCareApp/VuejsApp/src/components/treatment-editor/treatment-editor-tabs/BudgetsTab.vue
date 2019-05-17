484abe4458e9de0efec941a7984b3e770e4774ce
Paul Patorno <ppator1@hotmail.com>
2019-05-17 17:05:15 +0000

    Feature/216: TreatmentEditor Budgets Tab (#231)
    * -added BudgetsTab component html, script, & style code
    -modified TreatmentEditor to get the scenario's investment library data when a treatment has been selected
    -added a BudgetGridRow model for the data table in the BudgetsTab component
    
    * -added InvestmentLibrary data property "tabScenarioInvestmentLibrary" to TabData interface
    -modified getScenarioInvestmentLibrary action to set scenarioInvestmentLibrary in investment-editor.module to an emptyInvestmentLibrary object when the selectedScenarioId is 0
    -modified BudgetsTab component to use a watcher on selectedBudgets array in order to update the selected treatment's budgets when there is a change in selection
    -modified TreatmentEditor to clear the scenarioInvestmentLibrary from state if there is no selected scenario id or a user is navigating from the scenario treatment library editor to the treatment library editor (no scenario context)

---
+++ BridgeCareApp/VuejsApp/src/components/treatment-editor/treatment-editor-tabs/BudgetsTab.vue
@@ -0,0 +1,123 @@
+<template>
+    <v-container fluid grid-list-xl>
+        <v-layout class="budgets-tab-content">
+            <v-flex xs12>
+                <v-layout justify-center>
+                    <v-flex xs6>
+                        <v-layout v-if="budgets.length === 0" column fill-height>
+                            <h3>Investment Library Not Found</h3>
+                            <div>
+                                No investment library data was found for the selected scenario.
+                            </div>
+                            <div>
+                                To add investment library data, go to the scenario's investment editor.
+                            </div>
+                        </v-layout>
+                        <v-layout v-else fill-height>
+                            <v-data-table :items="budgets" :headers="budgetHeaders" v-model="selectedBudgets" select-all
+                                          item-key="budget" hide-actions
+                                          class="elevation-1 fixed-header v-table__overflow budgets-data-table">
+                                <template slot="items" slot-scope="props">
+                                    <td>
+                                        <v-checkbox v-model="props.selected" primary hide-details>
+                                        </v-checkbox>
+                                    </td>
+                                    <td>
+                                        {{props.item.budget}}
+                                    </td>
+                                </template>
+                            </v-data-table>
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop, Watch} from 'vue-property-decorator';
+    import {clone, isEmpty, findIndex, equals} from 'ramda';
+    import {TabData} from '@/shared/models/child-components/treatment-editor/tab-data';
+    import {
+        BudgetGridRow,
+        emptyTreatment,
+        emptyTreatmentLibrary,
+        Treatment,
+        TreatmentLibrary
+    } from '@/shared/models/iAM/treatment';
+    import {emptyInvestmentLibrary, InvestmentLibrary} from '@/shared/models/iAM/investment';
+    import {sortByProperty, sorter} from '@/shared/utils/sorter';
+    import {getPropertyValues} from '@/shared/utils/getter-utils';
+    import {DataTableHeader} from '@/shared/models/vue/data-table-header';
+
+    @Component
+    export default class BudgetsTab extends Vue {
+        @Prop() budgetsTabData: TabData;
+
+        budgetsTabSelectedTreatmentLibrary: TreatmentLibrary = clone(emptyTreatmentLibrary);
+        budgetsTabSelectedTreatment: Treatment = clone(emptyTreatment);
+        budgetsTabScenarioInvestmentLibrary: InvestmentLibrary = clone(emptyInvestmentLibrary);
+        budgetHeaders: DataTableHeader[] = [
+            {text: 'Budget', value: 'budget', align: 'left', sortable: true, class: '', width: '300px'}
+        ];
+        budgets: BudgetGridRow[] = [];
+        selectedBudgets: BudgetGridRow[] = [];
+
+        @Watch('budgetsTabData')
+        onBudgetsTabDataChanged() {
+            this.budgetsTabSelectedTreatmentLibrary = this.budgetsTabData.tabSelectedTreatmentLibrary;
+            this.budgetsTabSelectedTreatment = this.budgetsTabData.tabSelectedTreatment;
+            this.selectedBudgets = this.budgetsTabSelectedTreatment.budgets.map((name: string) => ({budget: name}));
+            this.budgetsTabScenarioInvestmentLibrary = this.budgetsTabData.tabScenarioInvestmentLibrary;
+        }
+
+        @Watch('budgetsTabScenarioInvestmentLibrary')
+        onScenarioInvestmentLibraryChanged() {
+            if (!isEmpty(this.budgetsTabScenarioInvestmentLibrary.budgetOrder)) {
+                this.budgets = this.budgetsTabScenarioInvestmentLibrary.budgetOrder.map((name: string) => ({budget: name}));
+            } else if (!isEmpty(this.budgetsTabScenarioInvestmentLibrary.budgetYears)) {
+                this.budgets = (sorter(
+                    getPropertyValues('budgetName', this.budgetsTabScenarioInvestmentLibrary.budgetYears)
+                ) as string[]).map((name: string) => ({budget: name}));
+            } else {
+                this.budgets = [];
+            }
+        }
+
+        @Watch('selectedBudgets')
+        onSelectedBudgetsChanged() {
+            const sortedTreatmentBudgets: BudgetGridRow[] = sortByProperty(
+                'budget', this.budgetsTabSelectedTreatment.budgets.map((name: string) => ({budget: name}))
+            );
+            const sortedSelectedBudgets: BudgetGridRow[] = sortByProperty('budget', this.selectedBudgets);
+            if (!equals(sortedSelectedBudgets, sortedTreatmentBudgets)) {
+                this.submitChanges();
+            }
+        }
+
+        /**
+         * Modifies the selected treatment & selected treatment library with budget selection changes and emits the
+         * modified objects to the parent component
+         */
+        submitChanges() {
+            this.budgetsTabSelectedTreatment.budgets = getPropertyValues('budget', this.selectedBudgets) as string[];
+
+            const index = findIndex((treatment: Treatment) =>
+                treatment.id === this.budgetsTabSelectedTreatment.id,
+                this.budgetsTabSelectedTreatmentLibrary.treatments
+            );
+            this.budgetsTabSelectedTreatmentLibrary.treatments[index] = this.budgetsTabSelectedTreatment;
+
+            this.$emit('submit', this.budgetsTabSelectedTreatmentLibrary);
+        }
+    }
+</script>
+
+<style>
+    .budgets-data-table {
+        height: 245px !important;
+        overflow-y: auto;
+    }
+</style>
