2c19d71564faffec73e064baebd6c752a1b22791
ppatorno <ppatorno@ara.com>
2019-05-29 12:46:27 -0500

    -added AttributeName interface -modified attribute.service.ts getAttributes function with API call -modified attribute.module to pluck 'name' properties from getAttributes response

--- BridgeCareApp/VuejsApp/src/services/attribute.service.ts
+++ BridgeCareApp/VuejsApp/src/services/attribute.service.ts
@@ -1,21 +1,12 @@
 import {AxiosPromise} from 'axios';
 import {axiosInstance} from '@/shared/utils/axios-instance';
-// TODO: remove MockAdapter code when api is implemented
-import MockAdapter from 'axios-mock-adapter';
-import {mockAttributes} from '@/shared/utils/mock-data';
+import {AttributeName} from '@/shared/models/iAM/attribute-name';
 
 export default class AttributeService {
     /**
      * Gets a list of attributes
      */
-    static getAttributes(): AxiosPromise<string[]> {
-        // TODO: remove MockAdapter code when api is implemented
-        const mockAdapterInstance = new MockAdapter(axiosInstance)
-            .onGet(`${axiosInstance.defaults.baseURL}/api/GetAttributes`)
-            .reply((config: any) => {
-                mockAdapterInstance.restore();
-                return [200, mockAttributes];
-            });
-        return axiosInstance.get<string[]>('/api/GetAttributes');
+    static getAttributes(): AxiosPromise<AttributeName[]> {
+        return axiosInstance.get<AttributeName[]>('/api/AttributeNames');
     }
 }
---
+++ BridgeCareApp/VuejsApp/src/shared/models/iAM/attribute-name.ts
@@ -0,0 +1,3 @@
+export interface AttributeName {
+    name: string;
+}
