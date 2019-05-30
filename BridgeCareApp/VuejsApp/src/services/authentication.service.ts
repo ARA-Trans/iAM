94c49b385b7699259b8c922697b8efd1e0d25f0a
Paul Patorno <ppator1@hotmail.com>
2019-05-30 14:46:43 +0000

    Feature/245: Authentication Service (#261)
    * -added an axios request interceptor and modified the response interceptor; handler will take care of showing/hiding the spinner (removed all setIsBusy action code from other components)
    -added/modified function comments for more clarity
    -moved/renamed files for better directory structure and naming
    -began process of removing console.log code
    
    * -refactored some of the services code while implementing axios error handling
    -refactored some of the models used to reflect changes in the swagger-hub api document
    
    * -added additional check for response.data in axios errorHandler
    -renamed security.module to authentication.module
    -renamed path query for simulationId to selectedScenarioId
    -added an axios-instance.ts file to use as a single instance import for all services
    -added a mock-adapter-instance.ts to use as a single MockAdapter instance import for all services without implemented APIs
    -removed simulation.service.ts
    -removed all instance of setIsBusy action in components
    -added committed-projects.service.ts file for committed projects uploader
    
    * -added code in all service files that is api ready
    -added an axios instance util that can be used app wide (allows base url to be changed easily)
    -added the axios-mock-adapter package to mock axios calls where APIs have not yet been developed
    
    * -added core-js for those who need it
    
    * -linting and comment corrections
    
    * -removed reports.module
    -added js-file-download for downloading reports
    -removed names property from ReportsDownloaderDialogData interface
    -modified reports.service functions to set responseType as 'blob'
    -moved reports.module action logic into ReportsDownloadDialog and modified it to use the js-file-download library to download the reports directly from the dialog
    
    * -added a CreateSimulationDataModel on the backend and removed the CreateSimulationData class from SimulationModel.cs
    
    * -modified route name for windows authorization based on api document
    -added UserInformationModel for better handling of data transfer
    -removed mock adapter and replace with actual api call in authenticationl.service.ts

--- BridgeCareApp/VuejsApp/src/services/authentication.service.ts
+++ BridgeCareApp/VuejsApp/src/services/authentication.service.ts
@@ -1,8 +1,6 @@
 ﻿import {AxiosPromise} from 'axios';
 import {UserInformation} from '@/shared/models/iAM/user-information';
 import {axiosInstance} from '@/shared/utils/axios-instance';
-// TODO: remove MockAdapter code when api is implemented
-import MockAdapter from 'axios-mock-adapter';
 
 
 export default class AuthenticationService {
@@ -10,13 +8,6 @@ export default class AuthenticationService {
      * Authenticates a user
      */
     static authenticateUser(): AxiosPromise<UserInformation> {
-        // TODO: remove MockAdapter code when api is implemented
-        const mockAdapterInstance = new MockAdapter(axiosInstance)
-            .onGet(`${axiosInstance.defaults.baseURL}/auth/AuthenticateUser`)
-            .reply((config: any) => {
-                mockAdapterInstance.restore();
-                return [200, {name: 'John Smith', id: '0'} as UserInformation];
-            });
         return axiosInstance.get<UserInformation>('/auth/AuthenticateUser', {withCredentials: true});
     }
 }
