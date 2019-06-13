331ef74d704f134c408f9e317a1354fa960f1413
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-06-13 17:20:23 -0500

    cleaning up app.js. Added config file for connection string, mongoose and socketIO

--- BridgeCareApp/BridgeCareNodeApp/app.js
+++ BridgeCareApp/BridgeCareNodeApp/app.js
@@ -2,61 +2,34 @@
 const express = require("express");
 const debug = require('debug')('app');
 const cors = require('cors');
-const mongoose = require("mongoose");
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('tiny'));
 app.use(compression());
 
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const server = app.listen(port, () => {
-  debug(`Running on port ${port}`);
+const config = require('./config/config')[env];
+require('./config/mongoose')(config);
+
+const server = app.listen(config.port, () => {
+  debug(`Running on port ${config.port}`);
 });
 
-const io = require('socket.io')(server);
+const io = require('./config/socketIO')(server);
 
 run().catch(error => console.error(error));
 
 async function run() {
-  let connectionTest = process.env.NODE_ENV;
-  if (process.env.NODE_ENV == 'development') {
-    await mongoose.connect("mongodb://sbhardwaj:BridgecareARA123@localhost:27017/BridgeCare?replicaSet=rs0", { useNewUrlParser: true })
-      .then(() => {
-        connectionTest = 'connected to mongo db on the local';
-        debug('connected to mongo db on the local');
-      })
-      .catch((err) => {
-        connectionTest = 'error has occured in connection';
-        debug('error has occured in connection');
-      });
-  }
-  if (process.env.NODE_ENV == 'production') {
-    await mongoose.connect("mongodb://admin:BridgecareARA123@localhost:27017/BridgeCare?replicaSet=r1", { useNewUrlParser: true })
-      .then(() => debug('connected to mongo db on the server'))
-      .then(() => {
-        connectionTest = 'connected to mongo db on the server';
-        debug('connected to mongo db on the server');
-      })
-      .catch((err) => {
-        connectionTest = 'server: error has occured in connection';
-        debug('server: error has occured in connection');
-      });
-
-  }
-
-  io.on('connect', (socket) => { debug('a user is connected'); });
-  io.on('disconnect', () => { debug('a user is disconnected'); });
 
   const InvestmentLibrary = require('./models/investmentLibraryModel');
-  const investmentLibraryrouter = require('./routes/investmentLibraryRouters')(InvestmentLibrary, connectionTest);
+  const investmentLibraryrouter = require('./routes/investmentLibraryRouters')(InvestmentLibrary);
 
   const Scenario = require('./models/scenarioModel');
   const scenarioRouter = require('./routes/scenarioRouters')(Scenario);
 
   app.use(cors());
-  io.origins('*:*');
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
