36cdc6e181416d9587c2e8556876441b896b4dfe
ppatorno <ppatorno@ara.com>
2019-07-23 09:18:53 -0500

    -modified ISectionLocator.Locate function to return just the SectionLocationModel instead of an IQueryable of the model -parameterized the raw sql of the SectionLocatorDAL.Locate function

--- BridgeCareApp/BridgeCare/DataAccessLayer/SectionLocatorDAL.cs
+++ BridgeCareApp/BridgeCare/DataAccessLayer/SectionLocatorDAL.cs
@@ -13,16 +13,42 @@ namespace BridgeCare.DataAccessLayer
         {
         }
 
-        public IQueryable<SectionLocationModel> Locate(SectionModel section, BridgeCareContext db)
+        public SectionLocationModel Locate(SectionModel section, BridgeCareContext db)
         {
-            var rawQueryForData = Enumerable.Empty<SectionLocationModel>();
             try
             {
                 // Including isnull statments to change these to '0' to avoid an
                 // exception as mapping a null to a non nullable data type
                 // crashes the entity framwork
-                var select = String.Format("SELECT Sectionid,isnull(Lat,0) as Latitude,isnull(Long,0) as Longitude FROM Segment_{0}_NS0 WHERE SectionId = {1}", section.NetworkId, section.SectionId);
-                rawQueryForData = db.Database.SqlQuery<SectionLocationModel>(select);
+                var query =
+                    $"SELECT Sectionid, isnull(Lat,0) as Latitude, isnull(Long,0) as Longitude FROM Segment_{section.NetworkId}_NS0 WHERE SectionId = @sectionId";
+                // create and open db connection
+                var connection = new SqlConnection(db.Database.Connection.ConnectionString);
+                connection.Open();
+                // create a sql command with the query and connection
+                var sqlCommand = new SqlCommand(query, connection);
+                // add parameter to sqlCommand
+                sqlCommand.Parameters.Add(new SqlParameter()
+                {
+                    ParameterName = "@sectionId",
+                    Value = section.SectionId
+                });
+                // create data reader from the sql command
+                var dataReader = sqlCommand.ExecuteReader();
+                // check if data reader has rows
+                if (dataReader.HasRows)
+                {
+                    // if data reader can execute a Read, then get the section location details and return
+                    if (dataReader.Read())
+                    {
+                        return new SectionLocationModel()
+                        {
+                            SectionId = dataReader.GetFieldValue<int>(0),
+                            Latitude = dataReader.GetFieldValue<double>(1),
+                            Longitude = dataReader.GetFieldValue<double>(2)
+                        };
+                    }
+                }
             }
             catch (SqlException ex)
             {
@@ -32,7 +58,11 @@ namespace BridgeCare.DataAccessLayer
             {
                 HandleException.OutOfMemoryError(ex);
             }
-            return rawQueryForData.AsQueryable();
+            catch (Exception ex)
+            {
+                HandleException.GeneralError(ex);
+            }
+            return new SectionLocationModel();
         }
     }
 }
