512a03314775b3ef4c57ea25e4499b0111c2b1ec
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2019-12-30 13:55:33 -0600

    updated nuget pacakages to their latest versions

--- BridgeCareApp/BridgeCare/App_Start/UnityWebApiActivator.cs
+++ BridgeCareApp/BridgeCare/App_Start/UnityWebApiActivator.cs
@@ -8,19 +8,18 @@ using Unity.AspNet.WebApi;
 namespace BridgeCare
 {
     /// <summary>
-    /// Provides the bootstrapping for integrating Unity with WebApi when it is
-    /// hosted in ASP.NET.
+    /// Provides the bootstrapping for integrating Unity with WebApi when it is hosted in ASP.NET.
     /// </summary>
     public static class UnityWebApiActivator
     {
         /// <summary>
         /// Integrates Unity when the application starts.
         /// </summary>
-        public static void Start()
+        public static void Start() 
         {
-            // Use UnityHierarchicalDependencyResolver if you want to use a new
-            // child container for each IHttpController resolution. var resolver
-            // = new UnityHierarchicalDependencyResolver(UnityConfig.Container);
+            // Use UnityHierarchicalDependencyResolver if you want to use
+            // a new child container for each IHttpController resolution.
+            // var resolver = new UnityHierarchicalDependencyResolver(UnityConfig.Container);
             var resolver = new UnityDependencyResolver(UnityConfig.Container);
 
             GlobalConfiguration.Configuration.DependencyResolver = resolver;
