d2e36fac155a569eceb177a105c9f6e46517525d
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2020-02-04 16:12:23 -0600

    added static data in the summary report

--- BridgeCareApp/BridgeCare/Services/SummaryReport/SummaryReportParameters.cs
+++ BridgeCareApp/BridgeCare/Services/SummaryReport/SummaryReportParameters.cs
@@ -50,12 +50,203 @@ namespace BridgeCare.Services.SummaryReport
             excelHelper.ApplyBorder(worksheet.Cells[1, 1, 1, 10]);
             // End of Simulation Name format
 
+            FillStaticData(worksheet);
+
             FillSimulationDetails(worksheet, investmentPeriod, inflationAndInvestments.InflationRate);
             FillAnalysisDetails(worksheet, investmentPeriod);
             FillJurisdictionCriteria(worksheet, investmentPeriod.Criteria);
             FillPriorities(worksheet, priorities);
             FillInvestmentAndBudgetCriteria(worksheet, inflationAndInvestments, criterias);
             worksheet.Cells.AutoFitColumns(50);
+        }
+
+        private void FillStaticData(ExcelWorksheet worksheet)
+        {
+            worksheet.Cells["A3:B3"].Value = "BridgeCare Rules Creator:";
+            excelHelper.MergeCells(worksheet, 3, 1, 3, 2);
+            worksheet.Cells["C3"].Value = "Central Office";
+            worksheet.Cells["A4:B4"].Value = "BridgeCare Rules Creator:";
+            excelHelper.MergeCells(worksheet, 4, 1, 4, 2);
+            excelHelper.ApplyBorder(worksheet.Cells[3, 1, 4, 2]);
+
+            excelHelper.MergeCells(worksheet, 6, 1, 6, 2);
+            worksheet.Cells["A6:B6"].Value = "NHS";
+            worksheet.Cells["A7"].Value = "NHS";
+            worksheet.Cells["A8"].Value = "Non-NHS";
+            worksheet.Cells["B7"].Value = "Y";
+            worksheet.Cells["B8"].Value = "Y";
+            excelHelper.ApplyBorder(worksheet.Cells[6, 1, 8, 2]);
+
+            excelHelper.MergeCells(worksheet, 10, 1, 10, 2);
+            worksheet.Cells["A10:B10"].Value = "6A19 BPN";
+            worksheet.Cells["A11"].Value = "1";
+            worksheet.Cells["A12"].Value = "2";
+            worksheet.Cells["A13"].Value = "3";
+            worksheet.Cells["A14"].Value = "4";
+            worksheet.Cells["A15"].Value = "H";
+            worksheet.Cells["B11"].Value = "Y";
+            worksheet.Cells["B12"].Value = "Y";
+            worksheet.Cells["B13"].Value = "Y";
+            worksheet.Cells["B14"].Value = "Y";
+            worksheet.Cells["B15"].Value = "N";
+
+            worksheet.Cells["A16"].Value = "L";
+            worksheet.Cells["A17"].Value = "T";
+            worksheet.Cells["A18"].Value = "D";
+            worksheet.Cells["A19"].Value = "N";
+            worksheet.Cells["A20"].Value = "Blank";
+            worksheet.Cells["B16"].Value = "Y";
+            worksheet.Cells["B17"].Value = "N";
+            worksheet.Cells["B18"].Value = "N";
+            worksheet.Cells["B19"].Value = "Y";
+            worksheet.Cells["B20"].Value = "Y";
+            excelHelper.ApplyBorder(worksheet.Cells[10, 1, 20, 2]);
+
+            excelHelper.MergeCells(worksheet, 23, 1, 23, 2);
+            worksheet.Cells["A23:B23"].Value = "Bridge Length";
+            worksheet.Cells["A24"].Value = "8-20";
+            worksheet.Cells["A25"].Value = "NBIS Length";
+            worksheet.Cells["B24"].Value = "Y";
+            worksheet.Cells["B25"].Value = "Y";
+
+            excelHelper.MergeCells(worksheet, 27, 1, 27, 2);
+            worksheet.Cells["A27:B27"].Value = "Status";
+            worksheet.Cells["A28"].Value = "Open";
+            worksheet.Cells["A29"].Value = "Closed";
+            worksheet.Cells["A30"].Value = "P3";
+            worksheet.Cells["A31"].Value = "Posted";
+            worksheet.Cells["B28"].Value = "Y";
+            worksheet.Cells["B29"].Value = "N";
+            worksheet.Cells["B30"].Value = "N";
+            worksheet.Cells["B31"].Value = "Y";
+            excelHelper.ApplyBorder(worksheet.Cells[23, 1, 31, 2]);
+
+            excelHelper.MergeCells(worksheet, 14, 4, 14, 6);
+            worksheet.Cells["D14:F14"].Value = "5A21 Owner";
+
+            excelHelper.MergeCells(worksheet, 15, 4, 15, 5, false);
+            worksheet.Cells["D15:E15"].Value = "01 - State Highway Agency";
+            excelHelper.MergeCells(worksheet, 16, 4, 16, 5, false);
+            worksheet.Cells["D16:E16"].Value = "02 - County Highway Agency";
+            excelHelper.MergeCells(worksheet, 17, 4, 17, 5, false);
+            worksheet.Cells["D17:E17"].Value = "03 - Town or Township Highway Agency";
+            excelHelper.MergeCells(worksheet, 18, 4, 18, 5, false);
+            worksheet.Cells["D18:E18"].Value = "04 - City, Municipal Highway Agency or Borough";
+            excelHelper.MergeCells(worksheet, 19, 4, 19, 5, false);
+            worksheet.Cells["D19:E19"].Value = "11 - State Park, Forest or Reservation Agency";
+            excelHelper.MergeCells(worksheet, 20, 4, 20, 5, false);
+            worksheet.Cells["D20:E20"].Value = "12 - Local Park, Forest or Reservation Agency";
+            excelHelper.MergeCells(worksheet, 21, 4, 21, 5, false);
+            worksheet.Cells["D21:E21"].Value = "21 - Other State Agency";
+            excelHelper.MergeCells(worksheet, 22, 4, 22, 5, false);
+            worksheet.Cells["D22:E22"].Value = "25 - Other local Agency";
+            excelHelper.MergeCells(worksheet, 23, 4, 23, 5, false);
+            worksheet.Cells["D23:E23"].Value = "26 - Private (Other than Railroad)";
+            excelHelper.MergeCells(worksheet, 24, 4, 24, 5, false);
+            worksheet.Cells["D24:E24"].Value = "27 - Railroad";
+            excelHelper.MergeCells(worksheet, 25, 4, 25, 5, false);
+            worksheet.Cells["D25:E25"].Value = "31 - State Toll Authority";
+            excelHelper.MergeCells(worksheet, 26, 4, 26, 5, false);
+            worksheet.Cells["D26:E26"].Value = "32 - Local Toll Authority";
+            excelHelper.MergeCells(worksheet, 27, 4, 27, 5, false);
+            worksheet.Cells["D27:E27"].Value = "60 - Other Federal Agencies";
+            excelHelper.MergeCells(worksheet, 28, 4, 28, 5, false);
+            worksheet.Cells["D28:E28"].Value = "62 - Bureau of Indian Affairs";
+            excelHelper.MergeCells(worksheet, 29, 4, 29, 5, false);
+            worksheet.Cells["D29:E29"].Value = "64 - U.S. Forest Service";
+            excelHelper.MergeCells(worksheet, 30, 4, 30, 5, false);
+            worksheet.Cells["D30:E30"].Value = "66 - National Park Service";
+            excelHelper.MergeCells(worksheet, 31, 4, 31, 5, false);
+            worksheet.Cells["D31:E31"].Value = "68 - Bureau of Land Management";
+            excelHelper.MergeCells(worksheet, 32, 4, 32, 5, false);
+            worksheet.Cells["D32:E32"].Value = "69 - Bureau of Reclamation";
+            excelHelper.MergeCells(worksheet, 33, 4, 33, 5, false);
+            worksheet.Cells["D33:E33"].Value = "70 - Military Reservation Corps Engineers";
+            excelHelper.MergeCells(worksheet, 34, 4, 34, 5, false);
+            worksheet.Cells["D34:E34"].Value = "80 - Unknown";
+            excelHelper.MergeCells(worksheet, 35, 4, 35, 5, false);
+            worksheet.Cells["D35:E35"].Value = "XX - Demolished/Replaced";
+
+            worksheet.Cells["F15"].Value = "Y";
+            worksheet.Cells["F16"].Value = "N";
+            worksheet.Cells["F17"].Value = "N";
+            worksheet.Cells["F18"].Value = "N";
+            worksheet.Cells["F19"].Value = "N";
+            worksheet.Cells["F20"].Value = "N";
+            worksheet.Cells["F21"].Value = "N";
+            worksheet.Cells["F22"].Value = "N";
+            worksheet.Cells["F23"].Value = "N";
+            worksheet.Cells["F24"].Value = "N";
+            worksheet.Cells["F25"].Value = "N";
+            worksheet.Cells["F26"].Value = "N";
+            worksheet.Cells["F27"].Value = "N";
+            worksheet.Cells["F28"].Value = "N";
+            worksheet.Cells["F29"].Value = "N";
+            worksheet.Cells["F30"].Value = "N";
+            worksheet.Cells["F31"].Value = "N";
+            worksheet.Cells["F32"].Value = "N";
+            worksheet.Cells["F33"].Value = "N";
+            worksheet.Cells["F34"].Value = "N";
+            worksheet.Cells["F35"].Value = "N";
+            excelHelper.ApplyBorder(worksheet.Cells[14, 4, 35, 6]);
+
+            excelHelper.MergeCells(worksheet, 14, 8, 14, 10);
+            worksheet.Cells["H14:J14"].Value = "5C22 Functional Class";
+            excelHelper.MergeCells(worksheet, 15, 8, 15, 10);
+            worksheet.Cells["H15:J15"].Value = "Rural";
+
+            excelHelper.MergeCells(worksheet, 16, 8, 16, 9, false);
+            worksheet.Cells["H16:I16"].Value = "01 - Principal Arterial - Interstate";
+            excelHelper.MergeCells(worksheet, 17, 8, 17, 9, false);
+            worksheet.Cells["H17:I17"].Value = "02 - Principal Arterial - Other";
+            excelHelper.MergeCells(worksheet, 18, 8, 18, 9, false);
+            worksheet.Cells["H18:I18"].Value = "06 - Minor Arterial";
+            excelHelper.MergeCells(worksheet, 19, 8, 19, 9, false);
+            worksheet.Cells["H19:I19"].Value = "07 - Major Collector";
+            excelHelper.MergeCells(worksheet, 20, 8, 20, 9, false);
+            worksheet.Cells["H20:I20"].Value = "08 - Minor Collector";
+            excelHelper.MergeCells(worksheet, 21, 8, 21, 9, false);
+            worksheet.Cells["H21:I21"].Value = "09 - Local";
+            excelHelper.MergeCells(worksheet, 22, 8, 22, 9, false);
+            worksheet.Cells["H22:I22"].Value = "NN - Other";
+
+            worksheet.Cells["J16"].Value = "Y";
+            worksheet.Cells["J17"].Value = "Y";
+            worksheet.Cells["J18"].Value = "Y";
+            worksheet.Cells["J19"].Value = "Y";
+            worksheet.Cells["J20"].Value = "Y";
+            worksheet.Cells["J21"].Value = "Y";
+            worksheet.Cells["J22"].Value = "Y";
+
+            excelHelper.MergeCells(worksheet, 23, 8, 23, 10);
+            worksheet.Cells["H23:J23"].Value = "Urban";
+
+            excelHelper.MergeCells(worksheet, 24, 8, 24, 9, false);
+            worksheet.Cells["H24:J24"].Value = "11 - Principal Arterial - Interstate";
+            excelHelper.MergeCells(worksheet, 25, 8, 25, 9, false);
+            worksheet.Cells["H25:I25"].Value = "12 - Principal Arterial - Other Freeway & Expressways";
+            excelHelper.MergeCells(worksheet, 26, 8, 26, 9, false);
+            worksheet.Cells["H26:I26"].Value = "14 - Other Principal Arterial";
+            excelHelper.MergeCells(worksheet, 27, 8, 27, 9, false);
+            worksheet.Cells["H27:I27"].Value = "16 - Minor Arterial";
+            excelHelper.MergeCells(worksheet, 28, 8, 28, 9, false);
+            worksheet.Cells["H28:I28"].Value = "17 - Collector";
+            excelHelper.MergeCells(worksheet, 29, 8, 29, 9, false);
+            worksheet.Cells["H29:I29"].Value = "19 - Local";
+            excelHelper.MergeCells(worksheet, 30, 8, 30, 9, false);
+            worksheet.Cells["H30:I30"].Value = "NN - Other";
+            excelHelper.MergeCells(worksheet, 31, 8, 31, 9, false);
+            worksheet.Cells["H31:I31"].Value = "99 - Ramp";
+
+            worksheet.Cells["J24"].Value = "Y";
+            worksheet.Cells["J25"].Value = "Y";
+            worksheet.Cells["J26"].Value = "Y";
+            worksheet.Cells["J27"].Value = "Y";
+            worksheet.Cells["J28"].Value = "Y";
+            worksheet.Cells["J29"].Value = "Y";
+            worksheet.Cells["J30"].Value = "Y";
+            worksheet.Cells["J31"].Value = "Y";
+            excelHelper.ApplyBorder(worksheet.Cells[14, 8, 31, 10]);
         }
 
         private void FillSimulationDetails(ExcelWorksheet worksheet, SimulationAnalysisModel investmentPeriod, double? inflationRate)
