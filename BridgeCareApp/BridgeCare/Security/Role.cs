4ff8d6352c78193fdb188fe6a03c87e7a25e09da
John Malmberg <jmalmberg@ara.com>
2020-03-10 16:53:23 -0500

    Handle users with multiple roles and with non-bams roles

--- BridgeCareApp/BridgeCare/Security/Role.cs
+++ BridgeCareApp/BridgeCare/Security/Role.cs
@@ -6,5 +6,6 @@
         public const string DISTRICT_ENGINEER = "PD-BAMS-DBEngineer";
         public const string PLANNING_PARTNER = "PD-BAMS-PlanningPartner";
         public const string CWOPA = "PD-BAMS-CWOPA";
+        public static string[] AllValidRoles = { ADMINISTRATOR, DISTRICT_ENGINEER, PLANNING_PARTNER, CWOPA };
     }
 }
--- BridgeCareApp/VuejsApp/src/shared/utils/parse-ldap.ts
+++ BridgeCareApp/VuejsApp/src/shared/utils/parse-ldap.ts
@@ -1,7 +1,16 @@
 /**
- * Given an LDAP-formatted string, returns the Common Name (CN) field
+ * Given an LDAP-formatted string, returns a list of all Common Name (CN) fields
  * @param ldap An LDAP-formatted string
  */
 export const parseLDAP = (ldap: string) => {
-    return ldap.split(',')[0].split('=')[1];
+    return ldap.split('^').map(segment => segment.split(',')[0].split('=')[1]);
+};
+
+/**
+ * Checks an LDAP-formatted string for a specific Common Name (CN) field
+ * @param ldap An LDAP-formatted string
+ * @param cn The CN to look for
+ */
+export const checkLDAP = (ldap: string, cn: string) => {
+    return parseLDAP(ldap).indexOf(cn) >= 0;
 };
