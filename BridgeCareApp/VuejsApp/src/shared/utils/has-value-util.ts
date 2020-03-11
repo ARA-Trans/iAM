7a85a4433b8499be7902df3a4a541a2e62da5583
ppatorno <ppatorno@ara.com>
2020-03-11 12:18:50 -0500

    -removed ramda imports from has-value-util.ts

--- BridgeCareApp/VuejsApp/src/shared/utils/has-value-util.ts
+++ BridgeCareApp/VuejsApp/src/shared/utils/has-value-util.ts
@@ -1,5 +1,3 @@
-import {isNil, isEmpty} from 'ramda';
-
 /**
  * Whether or not the specified item has a value (is not null, is not undefined, and is not considered empty)
  * @param item The item to check
