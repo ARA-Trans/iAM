bd871078e100e43f988a478e98201a12f9743197
John Malmberg <jmalmberg@ara.com>
2020-03-12 15:09:05 -0500

    Create analysis project

---
+++ BridgeCareApp/Analysis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ARA.iAM.Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnalysisConfiguration configuration = DataPersistence.GetConfigurationById(91);
+
+            AnalysisResult result = configuration.Compile().Run();
+        }
+    }
+
+    class AnalysisConfiguration
+    {
+        public CompiledAnalysis Compile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CompiledAnalysis
+    {
+        public AnalysisResult Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class AnalysisResult
+    {
+
+    }
+}
