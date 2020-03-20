192eedecabb8400fdec859d488f0a931b3e3db2e
William Rummler <w.a.rummler@gmail.com>
2020-03-20 17:15:48 -0500

    Simplify code generation script.

--- BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate/grammar/grammar2code.bat
+++ BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate/grammar/grammar2code.bat
@@ -1,4 +1,3 @@
 rd /s /q code
-set classpath=.\antlr-4.8-complete.jar;%classpath%
-java org.antlr.v4.Tool CalculateEvaluate.g4 -o code -Dlanguage=CSharp -package AppliedResearchAssociates.CalculateEvaluate
+java -cp .\antlr-4.8-complete.jar;%classpath% org.antlr.v4.Tool CalculateEvaluate.g4 -o code -Dlanguage=CSharp -package AppliedResearchAssociates.CalculateEvaluate
 if %errorlevel%==0 exit
