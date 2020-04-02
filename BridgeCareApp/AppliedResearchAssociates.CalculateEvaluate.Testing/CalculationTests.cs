c1d741a990ed9696c4e48f2733f54edff631bcbc
William Rummler <w.a.rummler@gmail.com>
2020-04-02 10:06:24 -0500

    Add rest of calculation alternative tests.

--- BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate.Testing/CalculationTests.cs
+++ BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate.Testing/CalculationTests.cs
@@ -5,13 +5,99 @@ namespace AppliedResearchAssociates.CalculateEvaluate.Testing
 {
     public class CalculationTests
     {
+        private const double A = 23, B = 19;
+
+        [Test]
+        public void Addition()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"{A} + {B}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A + B));
+        }
+
+        [Test]
+        public void ConstantReference()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator("pi");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(Math.PI));
+        }
+
+        [Test]
+        public void Division()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"{A} / {B}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A / B));
+        }
+
+        [Test]
+        public void Grouping()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"({A})");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A));
+        }
+
         [Test]
         public void Invocation()
         {
             var compiler = new CalculateEvaluateCompiler();
-            var calculator = compiler.GetCalculator("log(42)");
+            var calculator = compiler.GetCalculator($"log({A})");
             var result = calculator(null);
-            Assert.That(result, Is.EqualTo(Math.Log(42)));
+            Assert.That(result, Is.EqualTo(Math.Log(A)));
+        }
+
+        [Test]
+        public void Multiplication()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"{A} * {B}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A * B));
+        }
+
+        [Test]
+        public void Negation()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"-{A}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(-A));
+        }
+
+        [Test]
+        public void NumericLiteral()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"{A}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A));
+        }
+
+        [Test]
+        public void ParameterReference()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            compiler.Parameters["PARAM"] = ParameterType.Number;
+            var calculator = compiler.GetCalculator("[param]");
+            var argument = new CalculatorArgument<double>();
+            argument.Numbers["PaRaM"] = A;
+            var result = calculator(argument);
+            Assert.That(result, Is.EqualTo(argument.Numbers["pArAm"]));
+        }
+
+        [Test]
+        public void Subtraction()
+        {
+            var compiler = new CalculateEvaluateCompiler();
+            var calculator = compiler.GetCalculator($"{A} - {B}");
+            var result = calculator(null);
+            Assert.That(result, Is.EqualTo(A - B));
         }
     }
 }
