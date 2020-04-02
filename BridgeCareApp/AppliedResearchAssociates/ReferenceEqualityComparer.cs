64dc394909ec0914c006b273c71d20ace897a57d
William Rummler <w.a.rummler@gmail.com>
2020-04-02 09:53:01 -0500

    Add basic reference-identity equality comparer.

---
+++ BridgeCareApp/AppliedResearchAssociates/ReferenceEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AppliedResearchAssociates
+{
+    public sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T> where T : class
+    {
+        public static ReferenceEqualityComparer<T> Instance { get; } = new ReferenceEqualityComparer<T>();
+
+        public bool Equals(T x, T y) => ReferenceEquals(x, y);
+
+        public int GetHashCode(T obj) => RuntimeHelpers.GetHashCode(obj);
+
+        private ReferenceEqualityComparer()
+        {
+        }
+    }
+}
