c290a6e6e8d4e72885d627d0fb197db6d5230a68
William Rummler <w.a.rummler@gmail.com>
2020-04-02 12:32:58 -0500

    Add CalculateEvaluate benchmark project.

---
+++ BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate.Benchmarking/CalculationBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace AppliedResearchAssociates.CalculateEvaluate.Benchmarking
+{
+    public class CalculationBenchmark
+    {
+        public IEnumerable<string> CalculationExpressions
+        {
+            get
+            {
+                yield return "250*[DECK_AREA]";
+            }
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(CalculationExpressions))]
+        public Calculator<double> GetCalculator(string expression) => Compiler.GetCalculator(expression);
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Compiler = new CalculateEvaluateCompiler();
+            Compiler.Parameters["deck_area"] = ParameterType.Number;
+        }
+
+        private CalculateEvaluateCompiler Compiler;
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.CalculateEvaluate.Benchmarking/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AppliedResearchAssociates.CalculateEvaluate.Benchmarking
+{
+    internal static class Program
+    {
+        private static void Main() => _ = BenchmarkRunner.Run<CalculationBenchmark>();
+    }
+}
