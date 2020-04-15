feb8e365bb9a2c1c6c635dac2f1283fef676d1db
William Rummler <w.a.rummler@gmail.com>
2020-04-15 17:34:07 -0500

    Add linear interpolation for PiecewiseEquation compilation.

--- BridgeCareApp/AppliedResearchAssociates.iAM.Simulation/PiecewiseEquation.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Simulation/PiecewiseEquation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -17,14 +18,53 @@ namespace AppliedResearchAssociates.iAM.Simulation
 
         protected override void Compile()
         {
-            Data.Clear();
+            var rawData = new SortedDictionary<int, double>();
             var match = Pattern.Match(Expression);
             foreach (var group in match.Groups.Cast<Group>().Skip(1))
             {
                 var datum = group.Value.Split(',');
                 var input = int.Parse(datum[0]);
                 var output = double.Parse(datum[1]);
-                Data.Add(input, output);
+                rawData.Add(input, output);
+            }
+
+            if (rawData.Count == 0)
+            {
+                throw new InvalidOperationException("Expression has no data.");
+            }
+
+            var firstIndex = 0;
+            var lastIndex = rawData.Count - 1;
+
+            var keys = rawData.Keys.ToArray();
+            var values = rawData.Values.ToArray();
+
+            foreach (var key in Static.BoundRange(keys[firstIndex], keys[lastIndex]))
+            {
+                var index = Array.BinarySearch(keys, key);
+                if (index >= 0)
+                {
+                    Data.Add(key, values[index]);
+                }
+                else
+                {
+                    var upperIndex = ~index;
+                    var lowerIndex = upperIndex - 1;
+
+                    var upperValue = values[upperIndex];
+                    var lowerValue = values[lowerIndex];
+
+                    var rise = upperValue - lowerValue;
+                    var run = upperIndex - lowerIndex;
+
+                    var m = rise / run;
+                    var x = key - lowerIndex;
+                    var b = lowerValue;
+
+                    var y = m * x + b;
+
+                    Data.Add(key, y);
+                }
             }
         }
 
