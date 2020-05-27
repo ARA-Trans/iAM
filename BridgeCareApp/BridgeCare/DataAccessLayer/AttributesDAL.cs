044f7f50d8dd4d3db83c6589fcab02b025009f76
ppatorno <ppatorno@ara.com>
2020-05-27 16:11:05 -0500

    -added API function 'GetAttributeSelectValues -added DAL GetAttributeSelectValues -added models: NetworkAttribute, AttributeSelectValues, AttributeSelectValuesResult -updated getErrorMessage function to check for a 'statusText' property -added action getATtributeSelectValues, mutator attributesSelectValuesMutator, and state property attributesSelectValues -modified CriteriaEditor to check for new attributes on a change to the selectSubCriteriaClause, and if a new value is found to call the GetAttributeSelectValues API to update the list of values (if any) for the selected attribute in state

--- BridgeCareApp/BridgeCare/DataAccessLayer/AttributesDAL.cs
+++ BridgeCareApp/BridgeCare/DataAccessLayer/AttributesDAL.cs
@@ -1,4 +1,5 @@
-﻿using BridgeCare.Interfaces;
+﻿using System;
+using BridgeCare.Interfaces;
 using BridgeCare.Models;
 using System.Collections.Generic;
 using System.Data;
@@ -24,6 +25,31 @@ namespace BridgeCare.DataAccessLayer
             }
 
             return db.Attributes.ToList().Select(a => new AttributeModel(a)).ToList();
+        }
+
+        public AttributeSelectValuesResult GetAttributeSelectValues(int networkId, string attribute, BridgeCareContext db)
+        {
+            if (!db.NETWORKS.Any(network => network.NETWORKID == networkId))
+                throw new RowNotInTableException($"Could not find network with id {networkId}");
+
+            var validAttributes = GetAttributes(db).Select(attr => attr.Name);
+            if (!validAttributes.Contains(attribute))
+                throw new RowNotInTableException($"{attribute} is not a valid attribute");
+
+            var countsQuery = $"SELECT COUNT(DISTINCT({attribute})) FROM SEGMENT_{networkId}_NS0";
+            var rawQueryForCounts = db.Database.SqlQuery<int>(countsQuery).AsQueryable();
+            var result = rawQueryForCounts.ToList()[0];
+
+            if (result == 0)
+                return new AttributeSelectValuesResult(new List<string>(), $"No values found for attribute {attribute}; use text input");
+
+            if (result > 100)
+                return new AttributeSelectValuesResult(new List<string>(), $"Number of values for attribute {attribute} exceeds 100; use text input");
+
+            var dataQuery = $"SELECT DISTINCT(CAST({attribute} AS VARCHAR(255))) FROM SEGMENT_{networkId}_NS0";
+            var rawQueryForData = db.Database.SqlQuery<string>(dataQuery).AsQueryable();
+
+            return new AttributeSelectValuesResult(rawQueryForData.ToList(), "Success");
         }
     }
 }
