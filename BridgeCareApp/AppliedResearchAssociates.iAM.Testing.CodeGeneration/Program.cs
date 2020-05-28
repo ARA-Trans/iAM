874c7f107998d4dddfb94fd490a0e7104fd94a5a
William Rummler <w.a.rummler@gmail.com>
2020-05-28 15:44:24 -0500

    Add tool stub for generating test code.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Testing.CodeGeneration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppliedResearchAssociates.iAM.Testing.CodeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
