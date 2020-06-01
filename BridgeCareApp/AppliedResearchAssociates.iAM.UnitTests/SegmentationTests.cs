35c74348907d160c3204c9ff21666c6449a12331
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2020-06-01 16:32:01 -0500

    added test cases for segmentation

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTests/SegmentationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DataMiner.Attributes;
+using AppliedResearchAssociates.iAM.Segmentation;
+using NUnit.Framework;
+
+namespace AppliedResearchAssociates.iAM.UnitTests
+{
+    [TestFixture]
+    public class SegmentationTests
+    {
+        [Test]
+        public void CreateNUmericSegmentForLinearLocation()
+        {
+            // Arrange
+            var attributeTestData = new List<AttributeDatum<double>>();
+            // Act
+            attributeTestData.Add(TestDataForAttribute.NumericAttributeDataLinearLocation);
+            var segmentedData = Segmenter.CreateSegmentsFromAttributeDataRecords(attributeTestData);
+            //Assert
+            foreach (var item in segmentedData)
+            {
+                Assert.That(item.SegmentationAttributeDatum, Is.EqualTo(TestDataForSegmentation.NumericSegmentLinearLocation.SegmentationAttributeDatum));
+            }
+        }
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTests/TestDataForSegmentation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.Segmentation;
+
+namespace AppliedResearchAssociates.iAM.UnitTests
+{
+    public static class TestDataForSegmentation
+    {
+        public static Segment<double> NumericSegmentLinearLocation = new Segment<double>
+            (TestDataForAttribute.NumericAttributeDataLinearLocation.Location, TestDataForAttribute.NumericAttributeDataLinearLocation);
+    }
+}
